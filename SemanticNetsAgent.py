189c01582620a1ab11e0115fc97fb2fd7415e29f
Shashwat2419 <shashwat2419@gmail.com>
2026-04-05 14:33:43 +0530

    update

--- SemanticNetsAgent.py
+++ SemanticNetsAgent.py
@@ -1,6 +1,4 @@
-#References used:
-#https://www.geeksforgeeks.org/python/missionaries-and-cannibals/
-#https://en.wikipedia.org/wiki/Missionaries_and_cannibals_problem
+#More details for this project can be found in SemanticNetsAgent.md
 
 
 from collections import deque
@@ -10,15 +8,6 @@ class SemanticNetsAgent:
         self.moves = [(1,0), (0,1), (2,0), (0,2), (1,1)]
 
     def solve(self, initial_sheep, initial_wolves):
-        #Add your code here! Your solve method should receive
-        #the initial number of sheep and wolves as integers,
-        #and return a list of 2-tuples that represent the moves
-        #required to get all sheep and wolves from the left
-        #side of the river to the right.
-        #
-        #If it is impossible to move the animals over according
-        #to the rules of the problem, return an empty list of
-        #moves.
         start = (initial_sheep, initial_wolves, 0, 0, 'L')
         goal = (0, 0, initial_sheep, initial_wolves, 'R')
 
